7bbd2e905584a1afd00cd6c65115d63a41e84ca1
Akash <akash.mathwani@myysports.com>
2023-04-19 17:56:57 +0530

    apple login fix

--- src/api/apple-login.ts
+++ src/api/apple-login.ts
@@ -5,7 +5,7 @@ import axios from "axios";
 export async function appleLogin(id_token: string): Promise<LoginData> {
   console.log(AUTHENTICATION_BASE_URL + "/v1/auth/login/apple");
   const res = await axios.post(
-    AUTHENTICATION_BASE_URL + "/v1/auth/login/google",
+    AUTHENTICATION_BASE_URL + "/v1/auth/login/apple",
     { token: id_token }
   );
   console.log(res?.data?.data);
