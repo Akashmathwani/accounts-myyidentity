a4120615353f68a00a36540b3da64b26fb95062b
Akash <akash.mathwani@myysports.com>
2023-04-20 17:11:02 +0530

    auth https url

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,9 +2,9 @@ export const STAMP_API_BASE_URL = "http://localhost:5004";
 // export const AUTHENTICATION_BASE_URL = "http://localhost:4002";
 // export const AUTHENTICATION_BASE_URL =
 //   "https://ea78-2405-201-5c09-6011-613f-4f0c-14b0-3631.ngrok-free.app";
-// export const AUTHENTICATION_BASE_URL = "http://15.206.149.25:3000";
-export const AUTHENTICATION_BASE_URL =
-  "http://nextg-LoadB-1PI7T9JZI5QRS-d05e086d5f9fb789.elb.ap-south-1.amazonaws.com:4000";
+// export const AUTHENTICATION_BASE_URL =
+//   "https://fefb-123-201-214-73.ngrok-free.app";
+export const AUTHENTICATION_BASE_URL = "https://auth.highfives.in";
 export const GOOGLE_CLIENT_ID =
   "729824482770-r2d7r3919pbrc3hl9sg6o7qo8k8bvsed.apps.googleusercontent.com";
 
